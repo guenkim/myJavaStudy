e96bf51caf3d0b2c41bc3689e4c3273325bd97fa
geun <kkaddus@naver.com>
2024-01-05 19:47:45 +0900

    first commit

---
+++ springboot/src/test/java/com/guen/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guen.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
